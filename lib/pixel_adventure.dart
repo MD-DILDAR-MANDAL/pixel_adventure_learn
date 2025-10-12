3fdd2d4987882555e0d9d2f6afbe4ba04393717e
md-dildar-mandal <dildarmandal335@gmail.com>
2025-10-12 11:23:07 +0530

    chore: level 1 display setup

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -1,3 +1,26 @@
-import 'package:flame/game.dart';
+import 'dart:async';
 
-class PixelAdventure extends FlameGame {}
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'package:pixel_adventure_learn/levels/level.dart';
+
+class PixelAdventure extends FlameGame {
+  late CameraComponent cam;
+  final world = Level();
+
+  @override
+  Color backgroundColor() => Color(0xFF211F30);
+
+  @override
+  FutureOr<void> onLoad() {
+    cam = CameraComponent.withFixedResolution(
+      world: world,
+      width: 640,
+      height: 360,
+    );
+    cam.viewfinder.anchor = Anchor.topLeft;
+    addAll([cam, world]);
+    return super.onLoad();
+  }
+}
